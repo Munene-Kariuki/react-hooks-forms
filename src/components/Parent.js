f9a73cc7a891dd4371006e58b399001e6c6eb890
Denis <denismunene999@gmail.com>
2022-07-16 12:53:27 +0300

    commit tree a2864a1a13d090a50af0d020839b555837c3b012

---
+++ src/components/Parent.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+import Form from './Form';
+import DisplayData from './DisplayData';
+
+function Parent() {
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Henry");
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value)
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value)
+    console.log(e.target.value)
+  }
+
+  return (
+    <div>
+      <Form      
+        firstName={firstName}
+        lastName={lastName}
+        handleFirstNameChange={handleFirstNameChange}
+        handleLastNameChange={handleLastNameChange}
+      />
+      <DisplayData firstName={firstName} lastName={lastName} />
+    </div>
+    
+  )
+}
+
+export default Parent
